0e9b3d652be203e48fc92727123244942c7536e1
Lukas Winkler <l.winkler23@mailbox.org>
2017-09-09 18:13:52 +0200

    patch out eval() from libs/ace/worker-javascript.js

--- copy.sh
+++ copy.sh
@@ -8,3 +8,7 @@ do
 cp node_modules/ace-builds/src/${i}.js libs/ace/
 done
 cp node_modules/ace-builds/LICENSE node_modules/ace-builds/README.md libs/ace/
+
+# Patch out eval()
+sed -e 's/eval("throw 0;" + str);/return true;/g' libs/ace/worker-javascript.js > tmp.js
+mv tmp.js libs/ace/worker-javascript.js
